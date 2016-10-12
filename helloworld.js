1e8f355265c8e7eef4e53742f4b6e83d297cb9ac
Azhor Ahai <debapriya.das@incture.com>
2016-10-12 20:15:41 +0530

    added the JS file

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+#!/bin/sh
+':' //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
+
+// command to give the node interpreter a call
+
+if(process.argv.length<3) {
+	console.log("Enter command variables");
+	}
+else
+	console.log("Hello World, you are ", process.argv.slice(2));
