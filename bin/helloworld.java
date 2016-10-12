a9f40b677c922d6dce9d50a81fe6a0c44aa1e445
Azhor Ahai <debapriya.das@incture.com>
2016-10-12 20:06:05 +0530

    Added executables

---
+++ bin/helloworld.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class helloworld {
+
+	public static void main(String[] argv) {
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hello World");
+		System.out.println(argv.length);
+	
+	}
+
+}
